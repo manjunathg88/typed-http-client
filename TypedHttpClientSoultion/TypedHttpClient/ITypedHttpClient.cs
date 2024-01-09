d090ed77e09959947c902fb7d93a1cd2741f2e7c
Manjunath Gangadhar <manjunathg88@gmail.com>
2024-01-10 00:11:28 +0530

    Add files via upload

---
+++ TypedHttpClientSoultion/TypedHttpClient/ITypedHttpClient.cs
@@ -0,0 +1,7 @@
+﻿namespace TypedHttpClientNamespace
+{
+    public interface ITypedHttpClient
+    {
+        Task<HttpResponseMessage> Execute(string uri, HttpMethod method, HttpContent? payload=null);
+    }
+}
